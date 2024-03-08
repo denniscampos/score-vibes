4115c8e6a34276c6615e68abc4056c671e7d3691
Dennis Campos <dennisjr89@gmail.com>
2024-03-07 22:42:27 -0500

    feat: add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
+import { Loading } from "@/components/loader";
 import { Scores } from "@/components/scores";
 import { Standings } from "@/components/standings";
 import { UpcomingGames } from "@/components/upcoming-games";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <div className="p-4">
       <h1>Score Vibes</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <Scores />
-        <UpcomingGames />
-        <Standings />
+        <Suspense fallback={<Loading />}>
+          <Scores />
+        </Suspense>
+
+        <Suspense fallback={<Loading />}>
+          <UpcomingGames />
+        </Suspense>
+
+        <Suspense fallback={<Loading />}>
+          <Standings />
+        </Suspense>
       </div>
     </div>
   );
