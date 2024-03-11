a787b46aebf1b76cfa1691cae07dd1fbf03f243e
Dennis Campos <dennisjr89@gmail.com>
2024-03-11 18:49:36 -0400

    fix: testing utc issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,23 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { format } from "date-fns";
+import { zonedTimeToUtc } from "date-fns-tz";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const formatDate = (date: Date) => {
-  return format(date, "MMM d, h:mm a");
+export const formatDate = (
+  dateString: string,
+  timeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone
+) => {
+  const date = new Date(dateString); // Parse the UTC date string
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+    timeZone,
+  }).format(date);
 };
